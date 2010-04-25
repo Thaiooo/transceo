ccf64035631ce9914bec7281b8e8c23804c23555
quoc.thai.phan <quoc.thai.phan@70c575b2-f08c-13b3-3f7e-97fb8d302db4>
2010-04-25 11:07:45 +0000

    model

--- trunk/Transceo/grails-app/domain/com/transceo/Administrator.groovy
+++ trunk/Transceo/grails-app/domain/com/transceo/Administrator.groovy
@@ -3,4 +3,9 @@ package com.transceo
 class Administrator {
 	String login
 	String passwword
+	
+	static constraints = {
+		login(nullable: true, blank: true)
+		password(nullable: true, blank: true)
+	}
 }
