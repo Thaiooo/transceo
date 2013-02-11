2f2a4ae873fd40d04e404b53fcc4e59658d79fd5
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@70c575b2-f08c-13b3-3f7e-97fb8d302db4>
2013-02-11 09:12:52 +0000

    ...

--- Transceo/config/transeo-mail-config.groovy
+++ Transceo/config/transeo-mail-config.groovy
@@ -2,8 +2,8 @@ grails {
 	   mail {
 	     host = "smtp.gmail.com"
 	     port = 465
-	     username = "reservation@transeo-paris.com"
-	     password = "Transeo2010"
+	     username = ""
+	     password = ""
 	     props = ["mail.smtp.auth":"true", 					   
 	              "mail.smtp.socketFactory.port":"465",
 	              "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
